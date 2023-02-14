4986668602e6c141617b071e08ec2a7d1f50d0f1
Lucas <l.freittas@outlook.com>
2023-02-14 15:50:12 -0300

    create user ok

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,32 @@
+const AppError = require('../utils/AppError');
+const { hash } = require('bcryptjs');
+const knex = require('../database/knex');
+
 class UsersController{
     async create(request, response){
-        return response.json('Rota Create Ativa!')
+        const { name, email, password, isAdmin} = request.body
+
+        if(!name || !email || !password){
+            throw new AppError('Preencha todos os campos!')
+        }
+
+        const emailChecker = await knex('users').where({email}).first();
+        if(emailChecker){
+            throw new AppError('Este email já está em uso.')
+        };
+
+        const hashedPassword = await hash(password, 8)
+
+        const newUser = {
+            name,
+            email,
+            password: hashedPassword,
+            admin: isAdmin || false
+        };
+
+        await knex('users').insert(newUser);
+
+        return response.json('Usuário criado com sucesso!')
     };
 
     async update(request, response){
