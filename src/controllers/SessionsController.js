0b50325c3e871ddf251bc0ada49cf4654c01b464
Lucas <l.freittas@outlook.com>
2023-02-15 09:38:07 -0300

    jwt

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,31 @@
+const AppError = require('../utils/AppError');
+const knex = require('../database/knex');
+const authConfig = require('../configs/auth');
+const { compare } = require('bcryptjs');
+const { sign } = require('jsonwebtoken')
+
 class SessionsController{
     async create(request, response){
-        return response.json('Rota Create Ativa!')
+        const { email, password } = request.body;
+        const user = await knex('users').where({email}).first();
+
+        if(!user){
+            throw new AppError('Email e/ou senha incorretos', 401)
+        };
+
+        const passwordCheck = await compare(password, user.password);
+
+        if(!passwordCheck){
+            throw new AppError('Email e/ou senha incorretos', 401)
+        };
+
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+        
+        return response.json({user, token})
     };
 
 };
